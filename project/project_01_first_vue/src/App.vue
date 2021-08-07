9727afade46149ae6114906da3ce86b58274d754
Administrator <taomee517@gmail.com>
2021-08-07 21:46:49 +0800

    insert first vue project and helloworld

---
+++ project/project_01_first_vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<Threshold></Threshold>
+	</div>
+</template>
+
+<script>
+	import Threshold from './components/Threshold'
+	export default{
+		name: 'App',
+	
+		components:{
+			Threshold
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ project/project_01_first_vue/src/components/Threshold.vue
@@ -0,0 +1,45 @@
+<template>
+	<!-- 组件的结构 -->
+	<div>
+		<ol>
+			<li
+				v-for="thresh in result"
+				v-bind:key="thresh.id"
+				class="threshold"
+			>	
+				{{thresh.field}}阈值：{{thresh.lower}} {{thresh.unit}} - {{thresh.upper}} {{thresh.unit}}
+			</li>
+		</ol>
+	</div>
+</template>
+
+<script>
+	// 组件交互相关的代码（数据，方法等）
+	export default {
+		name: 'Threshold',
+		data() {
+			return {
+				code: 200,
+				message: "成功",
+				result: [
+					{id: 1, field: "电压", unit: "V", lower: 5, upper: 36},
+					{id: 2, field: "频率", unit: "Hz", lower: 100, upper: 5000},
+					{id: 3, field: "温度", unit: "°C", lower: -10, upper: 50}
+				]
+			}
+		},
+		  
+		methods:{
+			// showUnit(target){
+			// 	alert(target.unit)
+			// }
+		}
+	}
+</script>
+
+<style>
+	/* 组件的样式 css */
+	.threshold{
+		color: chocolate;
+	}
+</style>
